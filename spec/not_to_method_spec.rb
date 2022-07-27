1d58fe641362c052f9a24fc134e4abbaf3d5811e
Oscar De La Lanza <oscardelalanza@gmail.com>
2022-07-27 16:42:53 -0700

    not_to method

---
+++ spec/not_to_method_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe 'not_to method' do
+  it 'checks that two values do not match' do
+    expect(5).not_to eq(10)
+    expect('hello').not_to eq('Hello')
+    expect([1, 2]).not_to eq([1, 2, 3])
+  end
+end
