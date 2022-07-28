29f01b9f5bbedc04898b263dadcb0f5ef0bd796c
Oscar De La Lanza <oscardelalanza@gmail.com>
2022-07-27 21:10:15 -0700

    recieve certain times

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -30,9 +30,10 @@ class Movie
 
   def start_shooting
     if actor.ready?
-      puts actor.act
-      puts actor.fall_off_ladder
-      puts actor.light_on_fire
+      actor.act
+      actor.fall_off_ladder
+      actor.light_on_fire
+      actor.act
     end
   end
 end
@@ -47,10 +48,13 @@ RSpec.describe Movie do
 
   describe '#start_shooting method' do
     it 'expects an actor to do 3 actions' do
+      # expect(stuntman).to receive(:light_on_fire).once
+      # expect(stuntman).to receive(:light_on_fire).exactly(1).times
+      expect(stuntman).to receive(:light_on_fire).at_most(1).times
+      # expect(stuntman).to receive(:act).twice
+      expect(stuntman).to receive(:act).at_least(2).times
       expect(stuntman).to receive(:ready?)
-      expect(stuntman).to receive(:act)
       expect(stuntman).to receive(:fall_off_ladder)
-      expect(stuntman).to receive(:light_on_fire)
       subject.start_shooting
     end
   end
